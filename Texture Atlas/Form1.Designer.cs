6980944ec5b974abe0919b850774608e9a82c82c
Darklight437 <darklight437@gmail.com>
2017-10-04 12:02:41 +1100

    added some base assets and not much else

--- Texture Atlas/Form1.Designer.cs
+++ Texture Atlas/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Texture_Atlas
 {
-    partial class Form1
+    partial class AtlasGen
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AtlasGen));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.TextOutput = new System.Windows.Forms.TextBox();
+            this.ButtonExport = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.WindowText;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(307, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(525, 400);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // TextOutput
+            // 
+            this.TextOutput.Location = new System.Drawing.Point(12, 12);
+            this.TextOutput.Multiline = true;
+            this.TextOutput.Name = "TextOutput";
+            this.TextOutput.Size = new System.Drawing.Size(278, 244);
+            this.TextOutput.TabIndex = 1;
+            this.TextOutput.TextChanged += new System.EventHandler(this.TextOutput_TextChanged);
+            // 
+            // ButtonExport
+            // 
+            this.ButtonExport.Location = new System.Drawing.Point(12, 481);
+            this.ButtonExport.Name = "ButtonExport";
+            this.ButtonExport.Size = new System.Drawing.Size(75, 23);
+            this.ButtonExport.TabIndex = 2;
+            this.ButtonExport.Text = "Export";
+            this.ButtonExport.UseVisualStyleBackColor = true;
+            this.ButtonExport.Click += new System.EventHandler(this.ButtonExport_Click);
+            // 
+            // AtlasGen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(844, 516);
+            this.Controls.Add(this.ButtonExport);
+            this.Controls.Add(this.TextOutput);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AtlasGen";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox TextOutput;
+        private System.Windows.Forms.Button ButtonExport;
     }
 }
 
--- Texture Atlas/Form1.cs
+++ Texture Atlas/Form1.cs
@@ -7,14 +7,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Texture_Atlas
 {
-    public partial class Form1 : Form
+    public partial class AtlasGen : Form
     {
-        public Form1()
+        public AtlasGen()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void TextOutput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if(e.GetType() == typeof(MouseEventArgs))
+            {
+                MouseEventArgs me = e as MouseEventArgs;
+                TextOutput.Text = me.Location.ToString();
+            }
+        }
+
+        private void ButtonExport_Click(object sender, EventArgs e)
+        {
+            //want to activate saving in here
+           
         }
     }
 }
